9e0b7a1f8f0554983e3c597ca09b293e5320b476
Bernard Aboba <bernard.aboba@gmail.com>
2017-01-24 14:42:56 -0800

    Update the specification date
    Update the specification date

--- respec-config.js
+++ respec-config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   specStatus: "CG-DRAFT",
   shortName: "ortc-api",
-  publishDate: "2017-01-20",
+  publishDate: "2017-01-24",
   editors: [
     {
       name: "Robin Raymond",
