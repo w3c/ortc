800a1c874e34737e058e36697fb8f6cc76929b73
Bernard Aboba <bernard.aboba@gmail.com>
2017-05-18 22:12:35 -0700

    Update dates

--- respec-config.js
+++ respec-config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   specStatus: "CG-DRAFT",
   shortName: "ortc-api",
-  publishDate: "2017-05-15",
+  publishDate: "2017-05-19",
   editors: [
     {
       name: "Robin Raymond",
@@ -97,7 +97,7 @@ var respecConfig = {
       "authors": [
         "P. Thatcher"
       ],
-      "status": "15 May 2017. Internet draft (work in progress)",
+      "status": "19 May 2017. Internet draft (work in progress)",
       "publisher": "IETF"
     },
     "RID": {
