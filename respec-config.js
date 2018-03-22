342b6779c3467ae72cf758841357f84d55bcd435
aboba <bernard.aboba@gmail.com>
2018-03-22 12:19:37 +0000

    Updated to myCaps section

--- respec-config.js
+++ respec-config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   specStatus: "CG-DRAFT",
   shortName: "ortc-api",
-  publishDate: "2018-03-21",
+  publishDate: "2018-03-22",
   edDraftURI: "https://w3c.github.io/ortc/",
   editors: [
     {
