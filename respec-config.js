7382d49fe049afe4cf731201cda61d3aaf6bcd44
Bernard Aboba <bernard.aboba@gmail.com>
2018-10-02 22:12:07 -0700

    Update publication date

--- respec-config.js
+++ respec-config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   specStatus: "CG-DRAFT",
   shortName: "ortc-api",
-  publishDate: "2018-09-17",
+  publishDate: "2018-10-03",
   edDraftURI: "https://w3c.github.io/ortc/",
   editors: [
     {
